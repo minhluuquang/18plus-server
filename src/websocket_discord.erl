a86067b46f0a86a5d4b6d0f6ecd104b5ff38485e
minhlq <minhlq0696@gmail.com>
2024-10-12 14:43:02 +0700

    fix

---
+++ src/websocket_discord.erl
@@ -0,0 +1,18 @@
+-module(websocket_discord).
+-behaviour(gen_server).
+
+%% Callbacks for `gen_server`
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init(Args) ->
+    erlang:error(not_implemented).
+
+handle_call(Request, From, State) ->
+    erlang:error(not_implemented).
+
+handle_cast(Request, State) ->
+    erlang:error(not_implemented).
